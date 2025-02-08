963d060346afed85513a4038ae5e70260c941728
ARBYZ148852228 <anonim15255@gmail.com>
2025-02-08 14:36:01 +0300

    123
    123

---
+++ karkas.py
@@ -0,0 +1,26 @@
+import pygame
+pygame.init()
+size = (1280, 720)
+screen = pygame.display.set_mode(size)
+screen = pygame.display.set_caption('Моя Игра')
+BACKGROUND = (255, 255, 255)
+screen.fill(BACKGROUND)
+FPS = 60
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+    clock.tick(FPS)
+    if event.type == pygame.QUIT:
+        running = False
+
+    screen.fill(BACKGROUND)
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
